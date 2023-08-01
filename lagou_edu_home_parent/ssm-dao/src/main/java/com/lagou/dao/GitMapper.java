c8148a3a3cc604d7fd1d2874aa3e038c52eec77e
XiaoGenBanA <141048399+XiaoGenBanA@users.noreply.github.com>
2023-08-01 21:37:08 +0800

    Update GitMapper.java

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/GitMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/GitMapper.java
@@ -3,4 +3,10 @@ package com.lagou.dao;
 public interface GitMapper {
     public void add();
     public void add2();
+    public void add3();
+    public void add4();
+    public void add5();
+    public void add6();
+    public void add7();
+    public void add8();
 }
